548b5989dddbb757ed76f5d0ff6c72e8199636e1
Odunosh Moses <34396268+Moscode@users.noreply.github.com>
2023-08-01 04:38:36 +0000

    setting up usellm services

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
+"use client"
+import useLLM from "usellm"
+
 export default function Home() {
+  const llm = useLLM("https://usellm.org/api/llmservice");
+  llm.chat({
+    messages: [{role:"user", content:"What are you?"}],
+    onSuccess: message:any => console.log(message)
+  });
+
   return (
     <main>
       <h1>Hello Interview Assistant</h1>
